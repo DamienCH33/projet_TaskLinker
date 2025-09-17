93f72845656d79305a43b93232780b4a54585e58
DamienCH33 <damienchauveau@hotmail.fr>
2025-09-17 10:29:41 +0200

    maj details

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -2,13 +2,14 @@
 
 namespace App\Entity;
 
-use App\Repository\ProjectRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\DBAL\Types\Types;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity()]
+#[UniqueEntity('title', message: 'Un projet avec ce titre existe déjà.')]
 class Project
 {
     #[ORM\Id]
